fb976eb6934a755fb1847c7028ad7a47c3e4b101
Frank Schmitz <frank.schmitz@rewe-digital.com>
2018-10-19 14:07:59 +0200

    Load Training Data

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # tensorflow-basics
 
-Little test project by following this tutorial
+Little test project by following this tutorial:
 
 https://www.datacamp.com/community/tutorials/tensorflow-tutorial
+
+You can download the training and testing data here:
+
+https://btsd.ethz.ch/shareddata/
+
+and place it in a folder called 'TrafficSigns'
--- tensorflow-basics.py
+++ tensorflow-basics.py
@@ -1,1 +1,18 @@
+# Import `tensorflow`
+import os
 import tensorflow as tf
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+
+# Initialize two constants
+x1 = tf.constant([1,2,3,4])
+x2 = tf.constant([5,6,7,8])
+
+# Multiply
+result = tf.multiply(x1, x2)
+
+# Initialize Session and run `result`
+with tf.Session('/gpu:0') as sess:
+  output = sess.run(result)
+  print(output)
