1ac54caba427514c6e3c411fb6e5566d77e85722
Frank Schmitz <frank.schmitz@rewe-digital.com>
2018-10-19 15:01:17 +0200

    Load and plot images

--- tensorflow-basics.py
+++ tensorflow-basics.py
@@ -1,18 +1,60 @@
 # Import `tensorflow`
 import os
 import tensorflow as tf
+import skimage
+from skimage import transform
+import numpy as np
+import matplotlib.pyplot as plt
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
+def load_data(data_directory):
+    directories = [d for d in os.listdir(data_directory)
+                   if os.path.isdir(os.path.join(data_directory, d))]
+    labels = []
+    images = []
+    for d in directories:
+        label_directory = os.path.join(data_directory, d)
+        file_names = [os.path.join(label_directory, f)
+                      for f in os.listdir(label_directory)
+                      if f.endswith(".ppm")]
+        for f in file_names:
+            images.append(skimage.data.imread(f))
+            labels.append(int(d))
+    return np.array(images), labels
 
-# Initialize two constants
-x1 = tf.constant([1,2,3,4])
-x2 = tf.constant([5,6,7,8])
+train_data_directory = os.path.join(os.path.dirname(__file__), "TrafficSigns/Training")
+test_data_directory = os.path.join(os.path.dirname(__file__), "TrafficSigns/Testing")
 
-# Multiply
-result = tf.multiply(x1, x2)
+images, labels = load_data(train_data_directory)
 
-# Initialize Session and run `result`
-with tf.Session('/gpu:0') as sess:
-  output = sess.run(result)
-  print(output)
+
+# Rescale the images in the `images` array
+images28 = [transform.resize(image, (28, 28)) for image in images]
+
+# Get the unique labels
+unique_labels = set(labels)
+
+# Initialize the figure
+plt.figure(figsize=(15, 15))
+
+# Set a counter
+i = 1
+
+# For each unique label,
+for label in unique_labels:
+    # You pick the first image for each label
+    image = images28[labels.index(label)]
+    # Define 64 subplots
+    plt.subplot(8, 8, i)
+    # Don't include axes
+    plt.axis('off')
+    # Add a title to each subplot
+    plt.title("Label {0} ({1})".format(label, labels.count(label)))
+    # Add 1 to the counter
+    i += 1
+    # And you plot this first image
+    plt.imshow(image)
+
+# Show the plot
+plt.show()
