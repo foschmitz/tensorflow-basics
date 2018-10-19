acdb3413142c6e9b732efb237258612f417410af
Frank Schmitz <frank.schmitz@rewe-digital.com>
2018-10-19 15:55:56 +0200

    Use test data to visualize results

--- tensorflow-basics.py
+++ tensorflow-basics.py
@@ -3,8 +3,10 @@ import os
 import tensorflow as tf
 import skimage
 from skimage import transform
+from skimage.color import rgb2gray
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
@@ -31,30 +33,81 @@ images, labels = load_data(train_data_directory)
 
 # Rescale the images in the `images` array
 images28 = [transform.resize(image, (28, 28)) for image in images]
+images28 = np.array(images28)
+# Convert `images28` to grayscale
+images28 = rgb2gray(images28)
+# Initialize placeholders
+x = tf.placeholder(dtype = tf.float32, shape = [None, 28, 28])
+y = tf.placeholder(dtype = tf.int32, shape = [None])
 
-# Get the unique labels
-unique_labels = set(labels)
+# Flatten the input data
+images_flat = tf.contrib.layers.flatten(x)
 
-# Initialize the figure
-plt.figure(figsize=(15, 15))
+# Fully connected layer
+logits = tf.contrib.layers.fully_connected(images_flat, 62, tf.nn.relu)
 
-# Set a counter
-i = 1
+# Define a loss function
+loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels = y,
+                                                                    logits = logits))
+# Define an optimizer
+train_op = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
 
-# For each unique label,
-for label in unique_labels:
-    # You pick the first image for each label
-    image = images28[labels.index(label)]
-    # Define 64 subplots
-    plt.subplot(8, 8, i)
-    # Don't include axes
+# Convert logits to label indexes
+correct_pred = tf.argmax(logits, 1)
+
+# Define an accuracy metric
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+
+
+print("images_flat: ", images_flat)
+print("logits: ", logits)
+print("loss: ", loss)
+print("predicted_labels: ", correct_pred)
+
+
+tf.set_random_seed(1234)
+sess = tf.Session()
+
+sess.run(tf.global_variables_initializer())
+
+for i in range(201):
+        print('EPOCH', i)
+        _, accuracy_val = sess.run([train_op, accuracy], feed_dict={x: images28, y: labels})
+        if i % 10 == 0:
+            print("Loss: ", loss)
+        print('DONE WITH EPOCH')
+
+# Load the test data
+test_images, test_labels = load_data(test_data_directory)
+
+# Transform the images to 28 by 28 pixels
+test_images28 = [transform.resize(image, (28, 28)) for image in test_images]
+
+test_images28 = rgb2gray(np.array(test_images28))
+
+# Pick 10 random images
+sample_indexes = random.sample(range(len(test_images28)), 10)
+sample_images = [test_images28[i] for i in sample_indexes]
+sample_labels = [test_labels[i] for i in sample_indexes]
+
+# Run the "correct_pred" operation
+predicted = sess.run([correct_pred], feed_dict={x: sample_images})[0]
+
+# Print the real and predicted labels
+print(sample_labels)
+print(predicted)
+
+# Display the predictions and the ground truth visually.
+fig = plt.figure(figsize=(10, 10))
+for i in range(len(sample_images)):
+    truth = sample_labels[i]
+    prediction = predicted[i]
+    plt.subplot(5, 2,1+i)
     plt.axis('off')
-    # Add a title to each subplot
-    plt.title("Label {0} ({1})".format(label, labels.count(label)))
-    # Add 1 to the counter
-    i += 1
-    # And you plot this first image
-    plt.imshow(image)
+    color='green' if truth == prediction else 'red'
+    plt.text(40, 10, "Truth:        {0}\nPrediction: {1}".format(truth, prediction),
+             fontsize=12, color=color)
+    plt.imshow(sample_images[i],  cmap="gray")
 
-# Show the plot
 plt.show()
